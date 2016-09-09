33d501785727e3d2cb43b15a27c3b47ba3036198
NP晨 <NP@DESKTOP-LAFKRN7>
2016-09-09 13:38:12 +0800

    添加项目文件。

---
+++ hello world/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
